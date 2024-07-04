ce376ddd7c215ef13438c45818de5c11a01b9cdb
mtluc <tienluc0811@gmail.com>
2024-07-04 13:17:32 +0700

    router api

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:controler/:action",
+        destination: "/api/:controler?action=:action",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
